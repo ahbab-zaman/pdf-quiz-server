ec758eead9f2272aa4a271230ca57b0f93a7f48d
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-06-20 02:12:43 +0600

    add limit storage

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -9,7 +9,12 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10 MB max file size
+  },
+});
 
 router.post("/upload", upload.single("pdf"), processPDF);
 
